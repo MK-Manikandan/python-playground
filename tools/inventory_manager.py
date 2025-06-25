d875cf17f00d7e7fccca416d241c130ce8e4dbc1
Manikandan M <manikantanm06mk@gmail.com>
2025-06-25 18:50:34 +0530

    Use tkinter for interface. Finish full functionality. Working properly'

--- tools/inventory_manager.py
+++ tools/inventory_manager.py
@@ -1,4 +1,7 @@
+''' a json file will be created automatically while execuation
+the same file will be used again if it is present in the same folder '''
 import json
+import tkinter as tk
 
 try:
     with open('data.json', 'r') as file:
@@ -6,43 +9,72 @@ try:
 except:
     inv = dict()
 
-def dict_inp():
-    print('Format: item_name<space>number\nEnter zero when finished\n')
-    while True:
-        inp=input()
-        if inp == '0':
-            break
-        inp = inp.split(' ')
-        inp[0] = inp[0].capitalize()
-        try:
-            inv[inp[0]] = inv.get(inp[0],0)+int(inp[1])
-        except:
-            print('Invalid input\n')
-            continue
+''' everytime the function is called, the label is removed, and the whole dictionary
+is printed again, to avoid overlapping '''
+def add_input():
+    try:
+        destroy_label()
+    except:
+        pass
+    item=item_input.get()
+    item=item.capitalize()
+    number=number_input.get()
+    inv[item]=inv.get(item,0)+int(number)
+    txt=''
+    for k,v in inv.items():
+        txt = txt + k + ' : ' + str(v) + '\n'
+    global show_label
+    show_label = tk.Label(root, text = txt)
+    show_label.grid(row=2, column=0)
 
 def show():
-    if len(inv)==0:
-        print('The list is empty')
-        return
-    for k, v in inv.items():
-        print(f'{k} : {v}')
+    txt=''
+    for k,v in inv.items():
+        txt = txt + k + ' : ' + str(v) + '\n'
+    global show_label
+    show_label = tk.Label(root, text = txt)
+    show_label.grid(row=2, column=0)
 
 def clear():
     global inv
     inv = dict()
+    show_label.destroy()
 
-while True:
-    print('0 - stop the program\n1 - enter more items\
-          \n2 - show the list\n3 - clear the list')
-    decision = input()
-    if decision == '0':
-        break
-    elif decision == '1':
-        dict_inp()
-    elif decision == '2':
-        show()
-    elif decision == '3':
-        clear()
+def destroy_label():
+    show_label.destroy()
+
+def close():
+    root.destroy()
+
+#creating display window
+root=tk.Tk()
+root.title('Inventory manager')
+root.geometry('600x600')
+
+
+#creating buttons
+show_button = tk.Button(root, text='Refresh', command=show)
+show_button.grid(row=0, column=0)
+input_button = tk.Button(root, text = 'Add item', command= add_input)
+input_button.grid(row=1, column=4)
+clear_button = tk.Button(root, text = 'Clear List', command=clear)
+clear_button.grid(row = 0, column = 1)
+close_button = tk.Button(root, text = 'Close', command = close)
+close_button.grid(row = 0, column = 2)
+
+#creating input
+item_input=tk.Entry(root)
+item_input.grid(row=1, column=1)
+item_label=tk.Label(root, text='item:')
+item_label.grid(row =1, column= 0)
+
+number_input=tk.Entry(root)
+number_input.grid(row=1, column= 3)
+number_label=tk.Label(root, text= '  number:')
+number_label.grid(row= 1,  column= 2)
+
+root.mainloop()
+
 with open('data.json', 'w') as file:
     json.dump(inv, file)
 
