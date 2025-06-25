5478b3414150a797d4c44dbfd14b967774869bf7
Manikandan M <manikantanm06mk@gmail.com>
2025-06-25 14:28:08 +0530

    Finish code. Working fine, with basic interface

--- tools/inventory_manager.py
+++ tools/inventory_manager.py
@@ -1,25 +1,50 @@
 import json
+
 try:
     with open('data.json', 'r') as file:
-        inv=json.load(file)
+        inv = json.load(file)
 except:
-    inv=dict()
+    inv = dict()
 
 def dict_inp():
     print('Format: item_name<space>number\nEnter zero when finished\n')
     while True:
         inp=input()
-        if inp=='0':
+        if inp == '0':
             break
-        inp=inp.split(' ')
-        inp[0]=inp[0].capitalize()
+        inp = inp.split(' ')
+        inp[0] = inp[0].capitalize()
         try:
-            inv[inp[0]]=inv.get(inp[0],0)+int(inp[1])
+            inv[inp[0]] = inv.get(inp[0],0)+int(inp[1])
         except:
             print('Invalid input\n')
             continue
 
-dict_inp()
+def show():
+    if len(inv)==0:
+        print('The list is empty')
+        return
+    for k, v in inv.items():
+        print(f'{k} : {v}')
 
+def clear():
+    global inv
+    inv = dict()
+
+while True:
+    print('0 - stop the program\n1 - enter more items\
+          \n2 - show the list\n3 - clear the list')
+    decision = input()
+    if decision == '0':
+        break
+    elif decision == '1':
+        dict_inp()
+    elif decision == '2':
+        show()
+    elif decision == '3':
+        clear()
 with open('data.json', 'w') as file:
     json.dump(inv, file)
+
+#make a function for output
+#last commit was save in to and load out of a file
