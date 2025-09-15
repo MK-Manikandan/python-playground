b351ffdc4cfd70731edf6cee8040458f34292ce8
Manikandan M <manikantanm06mk@gmail.com>
2025-09-15 14:21:42 +0530

    Add loop for over Add game start mechanism Add main loop first half (first batting instance)

--- mini-games/hand_cricket.py
+++ mini-games/hand_cricket.py
@@ -1,11 +1,58 @@
 from os import system
 from time import sleep
 import random
+import platform
 
 #to clear the screen
 def clear():
-    _ = system('cls')
+    if platform.system() == 'Windows':
+        _ = system('cls')
+    else:
+        _ = system('clear')
 clear()
+
+#Header
+def header():
+    print('\n╔══════════════════════╗')
+    print('║     Hand Cricket     ║')
+    print('╚══════════════════════╝\n')
+header()
+
+#Starting the game
+overs = 6
+wickets = 6
+print('Play - 1\nCustomize and play - 2')
+while True:
+    try:
+        game = int(input('Enter the choice: '))
+    except:
+        print('Enter a valid input (1 or 2)')
+        continue
+    if game in [1, 2]: break
+    else: print('Wrong input!!!  Try again.')
+if game == 1: pass
+elif game == 2:
+    while True:
+        try:
+            overs = int(input('Enter number of overs: '))
+        except:
+            print('Enter a valid input (number)')
+            continue
+        if overs < 1:
+            print('Enter a positive integer')
+            continue
+        break
+    while True:
+        try:
+            wickets = int(input('Enter number of wickets: '))
+        except:
+            print('Enter a valid input (number)')
+            continue
+        if overs < 1:
+            print('Enter a positive integer')
+            continue
+        break
+    
 
 #tossing the coin
 coin = ['h', 't']
@@ -38,10 +85,80 @@ if toss:
         else: print('Wrong input!!!  Try again.')
     if choice == 'ba': print('You chose batting')
     else: print('You chose bowling')
+    sleep(2)
 else:
+    #choosing reverse here because computer's batting is player's bowling
     if random.randint(1, 2) == 1:
-        choice = 'ba'
+        choice = 'bo'
         print('Computer chose batting')
     else:
-        choice = 'bo'
+        choice = 'ba'
         print('Computer chose bowling')
+    sleep(2)
+
+#One over for both bowling and batting
+def over(runs, what):
+    for i in range(1, 7):
+        if runs < 0: result(False)
+        if what == 'ba':
+            print(f'\nRuns : {runs}')
+        else:
+            print(f'\nRuns left to win : {runs}')
+        print(f'---Ball {i}---')
+        while True:
+            try:
+                bat = int(input('Your choice: '))
+            except:
+                print('Enter a valid input ( a number in 1 - 6)')
+                continue
+            if bat in range(7): break
+            else: print('Wrong input!!!  Try again.')
+        ball = random.randint(0, 6)
+        print(f'Computer\'s choice : {ball}')
+        sleep(0.5)
+        if bat == ball:
+            print('Out')
+            return runs, True
+        else:
+            if what == 'ba':
+                runs += bat
+            else:
+                runs -= ball
+    return runs, False
+
+def result(a):
+    if a:
+        print('Congratulations. You have won')
+        system('pause')
+        quit()
+    else:
+        print('Sorry, You have lost')
+        system('pause')
+        quit()
+
+#Main game loop
+player_runs = 0
+computer_runs = 0
+#batting
+if choice == 'ba':
+    for j in range(2):
+        wickets_left = wickets
+        clear()
+        header()
+        if j == 0:
+            print('\n⟬ Your Batting ⟭')
+        else:
+            print('\n⟬ Your Bowling ⟭')
+        for i in range(overs):
+            print(f'\n     Over {3}   (Wickets left : {3})\n-------------------------------------')
+            if j == 0:
+                player_runs, if_out = over(player_runs, 'ba')
+            else:
+                player_runs, if_out = over(player_runs, 'bo')
+            if if_out:
+                wickets_left -= 1
+            if wickets_left == 0:
+                print('Innings over')
+                break
+        print('----Innings over----')
+print(f'Player = {player_runs}\nComputer = {computer_runs}')
